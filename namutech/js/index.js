e25d1dcd3b24f51257b1d4feca4f95a8e789b924
moon4 <jellypob@naver.com>
2023-08-10 11:07:38 +0900

    서울시청,나무테크 링크 추가

---
+++ namutech/js/index.js
@@ -0,0 +1,13 @@
+$("#slider").slick({
+    arrows: false,
+
+    autoplay: true,
+    autoplaySpeed: 2000,
+
+})
+
+$(".corporateCompanyArea").find("#companySlide").slick({
+    slidesToShow: 5,
+    autoplay: true,
+    arrows: false,
+})
